8af3729d43231a29636a21f18b8634acdf2c45b9
Sesha Sai <96340159+Seshasai14@users.noreply.github.com>
2024-07-17 14:56:05 +0530

    Time: 907 ms (27.57%), Space: 28.8 MB (7.21%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
@@ -1,25 +1,14 @@
-from typing import List
-
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
-        n = len(nums)
-        if k == 1:
-            return max(nums)
+        L=0
+        windowSum=sum(nums[:k])
+        maxAvg,avg=windowSum/k,0
+        for R in range(k,len(nums)):
+            windowSum+=nums[R]-nums[L]
+            maxAvg=max(windowSum/k,maxAvg)
+            L+=1
+        return maxAvg
+
+
+
         
-        # Calculate the sum of the first subarray of length k
-        sum_window = sum(nums[:k])
-        max_avg = sum_window / k  # Initialize max_avg with the average of the first subarray
-        
-        # Iterate over the rest of the subarrays
-        for i in range(1, n - k + 1):
-            # Update the sum by subtracting the first element of the previous subarray
-            # and adding the next element
-            sum_window = sum_window - nums[i - 1] + nums[i + k - 1]
-            
-            # Calculate the average of the current subarray
-            avg = sum_window / k
-            
-            # Update max_avg if necessary
-            max_avg = max(max_avg, avg)
-        
-        return max_avg
